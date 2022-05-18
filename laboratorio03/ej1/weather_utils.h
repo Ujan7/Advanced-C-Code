749991e76a4ab914ea2a1a4983fcb84e3dca93db
Eugenio Arcana <105726386+Luny7@users.noreply.github.com>
2022-05-18 12:47:15 -0300

    Delete laboratorio03 directory

--- laboratorio03/ej1/weather_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WEATHER_UTILS
-#define _WEATHER_UTILS
-
-int minTempR(char *filepath);
-void maxTempPerYeaR(WeatherTable a, int output[YEARS]);
-void monthMaxPrecPerYear(WeatherTable a, int output[YEARS]);
-
-#endif
